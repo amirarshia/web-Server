dfb495db7c3a7cd85c02c7ff9f72bfa243f259c0
Chobin <amirarshia3155@gmail.com>
2022-03-20 15:13:18 -0400

    Added a 500 method, and changed out all of the unwrap for manual error handling

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,59 @@
 use std::fs;
 use std::io::Read;
 use std::io::Write;
+use std::net::IpAddr;
+use std::net::Ipv4Addr;
+use std::net::SocketAddr;
 use std::net::TcpListener;
+use std::net::TcpStream;
 use std::path::Path;
+use std::ptr;
 
 fn main() {
     println!("Hello World");
     let listener = TcpListener::bind("0.0.0.0:80").unwrap();
-    for stream in listener.incoming() {
+    for conn in listener.incoming() {
         println!("Incoming Connection [+]");
-        let mut stream = stream.unwrap();
-        println!("ip: {}", stream.peer_addr().unwrap());
+        let mut uri = "";
+        let mut stream;
+        match conn {
+            Ok(t) => stream = t,
+            _ => return,
+        };
+        let mut ip: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 80);
+        match stream.peer_addr() {
+            Ok(i) => ip = i,
+            _ => uri = "/500",
+        }
+        println!("ip: {}", ip);
         let mut buffer = [0; 1024];
         match stream.read(&mut buffer) {
             Ok(_) => {}
-            Err(_) => return,
+            Err(_) => uri = "/500",
         }
         println!("Request:\r\n{} ", String::from_utf8_lossy(&buffer[..]));
         let request = String::from_utf8_lossy(&buffer[..]);
-        let request_line = request.lines().next().unwrap();
-        let mut parts = request_line.split_whitespace();
-        let vc = request_line.split_whitespace().collect::<Vec<&str>>();
-        if vc.len() < 2 {
-            continue;
+        let mut request_line = "";
+        match request.lines().next() {
+            Some(r) => request_line = r,
+            None => uri = "/500",
         }
-        let method = parts.next().ok_or("Method not specified").unwrap();
-        let uri = Path::new(parts.next().ok_or("URI not specified").unwrap());
-        let uri_string = uri.as_os_str().to_str().unwrap();
-        println!("{}", method);
-        println!("{}", uri_string);
+        let mut parts = request_line.split_whitespace();
+        let mut method = "";
+        match parts.next() {
+            Some(p) => method = p,
+            None => {
+                println!("Method not specified");
+                uri = "/500"
+            }
+        }
+        match parts.next() {
+            Some(p) => uri = p,
+            None => {
+                println!("URI not specified");
+                uri = "/500"
+            }
+        }
         let home_content = String::from(
             fs::read_to_string(&"./www/home.html")
                 .unwrap_or(format!("reading file content failed [-]")),
@@ -41,11 +66,16 @@ fn main() {
             fs::read_to_string(&"./www/404.html")
                 .unwrap_or(format!("reading file content failed [-]")),
         );
+        let five00_content = String::from(
+            fs::read_to_string(&"./www/500.html")
+                .unwrap_or(format!("reading file content failed [-]")),
+        );
         let mut header = format!("{} {}", "HTTP/1.1", http::StatusCode::OK);
         let mut body = String::from("");
-        match uri_string {
+        match uri {
             "/" => body = index_content,
             "/home" => body = home_content,
+            "/500" => body = five00_content,
             _ => {
                 body = four04_content;
                 header = format!("{} {}", "HTTP/1.1", http::StatusCode::NOT_FOUND)
