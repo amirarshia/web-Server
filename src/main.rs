af8382b46b329c654b2284aab669534523ca9b7b
Chobin <amirarshia3155@gmail.com>
2022-03-24 19:59:18 -0400

    Switched into Actix framework and added register handler

--- src/main.rs
+++ src/main.rs
@@ -1,104 +1,29 @@
-use std::fs;
-use std::io::Read;
-use std::io::Write;
-use std::net::IpAddr;
-use std::net::Ipv4Addr;
-use std::net::SocketAddr;
-use std::net::TcpListener;
-use std::net::TcpStream;
-use std::path::Path;
-use std::ptr;
+use actix_files as fs;
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use serde_derive::{Deserialize, Serialize};
 
-fn main() {
-    println!("Hello World");
-    let listener = TcpListener::bind("0.0.0.0:8080").unwrap();
-    for conn in listener.incoming() {
-        println!("Incoming Connection [+]");
-        let mut uri = "";
-        let mut stream;
-        match conn {
-            Ok(t) => stream = t,
-            _ => return,
-        };
-        let mut ip: SocketAddr = SocketAddr::new(IpAddr::V4(Ipv4Addr::new(0, 0, 0, 0)), 80);
-        match stream.peer_addr() {
-            Ok(i) => ip = i,
-            _ => uri = "/500",
-        }
-        println!("IP: {}", ip);
-        let mut buffer = [0; 1024];
-        match stream.read(&mut buffer) {
-            Ok(_) => {}
-            Err(_) => uri = "/500",
-        }
-        println!("Request:\r\n{} ", String::from_utf8_lossy(&buffer[..]));
-        let request = String::from_utf8_lossy(&buffer[..]);
-        let mut request_line = "";
-        match request.lines().next() {
-            Some(r) => request_line = r,
-            None => uri = "/500",
-        }
-        let mut parts = request_line.split_whitespace();
-        let mut method = "";
-        match parts.next() {
-            Some(p) => method = p,
-            None => {
-                println!("Method not specified");
-                uri = "/500"
-            }
-        }
-        match parts.next() {
-            Some(p) => uri = p,
-            None => {
-                println!("URI not specified");
-                uri = "/500"
-            }
-        }
-        let home = String::from(
-            fs::read_to_string(&"./www/home.html")
-                .unwrap_or(format!("reading file content failed [-]")),
-        );
-        let index_content = String::from(
-            fs::read_to_string(&"./www/index.html")
-                .unwrap_or(format!("reading file content failed [-]")),       
-        );
-        let index_css = String::from(
-            fs::read_to_string(&"./css/styles.css")
-                .unwrap_or(format!("reading file content failed [-]")),       
-        );
-        let four04_content = String::from(
-            fs::read_to_string(&"./www/404.html")
-                .unwrap_or(format!("reading file content failed [-]")),
-        );
-        let five00_content = String::from(
-            fs::read_to_string(&"./www/500.html")
-                .unwrap_or(format!("reading file content failed [-]")),
-        );
-        let register = String::from(
-            fs::read_to_string(&"./www/register.html")
-                .unwrap_or(format!("reading file content failed [-]")),
-        );
-        let mut header = format!("{} {}", "HTTP/1.1", http::StatusCode::OK);
-        let mut body = String::from("");
-        match uri {
-            "/" => body = index_content,
-            "/css/styles.css" => body = index_css,
-            "/home" => body = home,
-            "/500" => body = five00_content,
-            "/register" => body = register,
-            _ => {
-                body = four04_content;
-                header = format!("{} {}", "HTTP/1.1", http::StatusCode::NOT_FOUND)
-            }
-        }
-        let response = format!("{} \r\n\r\n {}\r\n", header, body);
-        let streamWrite = match stream.write(response.as_bytes()) {
-            Ok(_) => {},
-            Err(_) => continue,
-        };
-        let streamFlush = match stream.flush() {
-            Ok(_) => println!("Request Completed [+]"),
-            Err(_) => continue,
-        };
-    }
+#[derive(Deserialize)]
+struct Register {
+    email: String,
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(register)
+            .service(fs::Files::new("/", "www").index_file("index.html"))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+#[post("/register")]
+async fn register(request_body: web::Form<Register>) -> impl Responder {
+    println!("Email Received: {}", request_body.email);
+    
+    HttpResponse::Found()
+        .append_header(("Location", "/register.html"))
+        .finish()
 }
